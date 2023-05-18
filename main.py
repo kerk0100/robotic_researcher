1b9354fea1516437a1ed8c73eb3739cd88255816
kerk0100 <lauren.kerker@hotmail.com>
2023-05-18 10:56:48 -0700

    formatting output

--- main.py
+++ main.py
@@ -1,42 +1,58 @@
 from robotics import Robot
 import pandas as pd
 
-# SCIENTISTS = ["Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin"]
-SCIENTISTS = ["Albert Einstein"]
+SCIENTISTS = ["Albert Einstein", "Isaac Newton", "Marie Curie", "Charles Darwin"]
+WIKI_URL = 'https://en.wikipedia.org/wiki/'
 
 robot = Robot("Quandrinaut")
 
 
+# robot says hello to user
 def introduce_yourself():
     robot.say_hello()
 
 
+# robot says goodbye to user
 def farewell_yourself():
     robot.say_goodbye()
 
 
-def get_webpage(url, scientist):
-    data = robot.open_webpage(url, scientist)
+# robot gets data from webpage and displays to user
+def get_data(url, scientist):
+    data = robot.get_data_from_webpage(url, scientist)
     return data
 
 
+# robot asks if user wants to save the data
 def ask_to_download(all_scientist_info):
-    answer = input("Would you like to download this data as a csv? Y/N")
+    answer = input("Would you like to save this data as a csv? Y/N \n")
     if answer == "Y":
         robot.download_data(all_scientist_info)
     else:
+        print("Data not saved.")
         return
 
 
 def main():
     introduce_yourself()
-    all_scientist_info = pd.DataFrame(columns=["Name", "Age", "First Paragraph"])
+
+    # create df to save the data collected on each scientist
+    all_scientist_info = pd.DataFrame(columns=["Name", "Age", "First Paragraph", "URL"])
+
+    # iterate through all scientists to collect data on each
     for scientist in SCIENTISTS:
+        print("Collecting data for: " + scientist + "...")
+        # create the URL
         first_name = scientist.split(" ")[0]
         last_name = scientist.split(" ")[1]
-        url = 'https://en.wikipedia.org/wiki/' + first_name + "_" + last_name
-        get_data = get_webpage(url, scientist)
-        all_scientist_info.loc[len(all_scientist_info)] = get_data
+        url = WIKI_URL + first_name + "_" + last_name
+
+        # get data on scientist
+        data = get_data(url, scientist)
+
+        # save data to df
+        all_scientist_info.loc[len(all_scientist_info)] = data
+
     ask_to_download(all_scientist_info)
     farewell_yourself()
 
--- robotics.py
+++ robotics.py
@@ -1,5 +1,7 @@
 from RPA.Browser.Selenium import Selenium
 from datetime import datetime
+import os
+import textwrap
 
 br = Selenium()
 
@@ -10,22 +12,43 @@ class Robot:
 
     def say_hello(self):
         print("Hello, my name is " + self.name)
+        print("I will provide you with some information about famous scientists.")
+        print('_' * 40)
 
     def say_goodbye(self):
-        print("Goodbye, my name is " + self.name)
+        print("Goodbye.")
 
-    def open_webpage(self, webpage, scientist):
-        br.open_available_browser(webpage)
+    def get_data_from_webpage(self, webpage, scientist):
+        # open browser in background
+        br.open_headless_chrome_browser(webpage)
+
+        # find birth and death dates
         date_born = br.find_element(locator="xpath://span[@class='bday']").get_attribute("textContent")
         date_died = br.find_elements(locator="xpath://td[@class='infobox-data']")[1].get_attribute("textContent")
         date_died = date_died[date_died.find("(")+1:date_died.find(")")]
         year_born = datetime.strptime(date_born, "%Y-%m-%d").year
         year_died = datetime.strptime(date_died, "%Y-%m-%d").year
         age = int(year_died) - int(year_born)
-        first_paragraph = br.find_elements(locator="xpath://p")[1].get_attribute("textContent")
-        scientist_data = {'Name': scientist, 'Age': age, 'First Paragraph': first_paragraph}
+
+        # find first paragraph
+        paragraphs = br.find_elements(locator="xpath://p")
+        first_paragraph = "Could not find first paragraph."
+        for p in paragraphs:
+            if p.text:
+                first_paragraph = textwrap.dedent(p.text).strip()
+                break
+
+        # print data to user
+        print("Age: " + str(age))
+        print("Info: " + textwrap.fill(first_paragraph, width=80))
+        print('_' * 40)
+
+        # save data for df
+        scientist_data = {'Name': scientist, 'Age': age, 'First Paragraph': first_paragraph, 'URL': webpage}
         return scientist_data
 
+    # save df to csv file and print out file path to user
     def download_data(self, all_scientist_info):
         all_scientist_info.to_csv("scientist_info.csv")
-        print("Scientist info has been saved to csv file with name: scientist_info.csv")
+        file_path = os.getcwd()
+        print("Scientist info has been saved to csv file with name:" + file_path + "/scientist_info.csv")
